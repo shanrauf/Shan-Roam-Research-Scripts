82d8e1c6d2df10f36e0a15a1d71204efedffbd5a
shanrauf <s.rauf124@gmail.com>
2021-08-31 09:20:16 -0700

    Add block sending UI

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import { runExtension } from './entry-helpers';
+import { roam42KeyboardLib } from './keyboard';
+import { getUids } from 'roam-client';
+
+declare global {
+  interface Window {
+    lastFocusedBlock: string | null;
+  }
+}
 
 const extensionId = 'roam-personal-scripts';
 
@@ -122,6 +130,13 @@ function resolveCompletedObject(
     resolvedObjectUid
   )[0];
 
+  if (!dependeeObjectUids) {
+    alert(
+      'Nothing depends on tis object; contact developer if this is a mistake'
+    );
+    return;
+  }
+
   dependeeObjectUids.forEach((dependeeUid) => {
     // Remove the resolved object from Depends On
     const dependsOnRefBlockUid = window.roamAlphaAPI.q(
@@ -166,8 +181,106 @@ function resolveCompletedObject(
     });
   });
 }
+function onShortcut(
+  callback: (pageUid: string, blockUid: string) => void
+): void {
+  const blockUid = window.roamAlphaAPI.ui.getFocusedBlock()?.['block-uid'];
+  if (!blockUid) return;
+
+  const pageUid = window.roamAlphaAPI.q(
+    `[:find ?page-uid :in $ ?block-uid :where [?b :block/uid ?block-uid] [?b :block/page ?p] [?p :block/uid ?page-uid]]`,
+    blockUid
+  )[0][0];
+
+  callback(pageUid, blockUid);
+}
+type SendType = 'raw' | 'ref';
+
+function onSendBlock(sendType: SendType): void {
+  // Select block from CtrlShift9 UI
+  roam42KeyboardLib.pressEnter(0);
+  // Wait a bit for the UI to add the ref to the block before trying to extract the ref
+  setTimeout(() => {
+    try {
+      const currentlyEditingBlock = document.querySelector(
+        'textarea.rm-block-input'
+      ) as HTMLTextAreaElement;
+      const currentBlockUid = getUids(currentlyEditingBlock)?.blockUid;
+
+      // Note: This may cause issues if selecting/highlighting text, if Roam updates block ref lengths (e.g. uids get longer), or if timing issues with Enter event and timeout
+      const sendToText = currentlyEditingBlock.textContent;
+      const sendToEnd = currentlyEditingBlock.selectionStart;
+      const sendToStart = sendToEnd - 13;
+      const sendToUid = sendToText
+        .slice(sendToStart, sendToEnd)
+        .replace('((', '')
+        .replace('))', '');
+      const sendToNewText =
+        sendToText.substring(0, sendToStart) +
+        sendToText.substring(sendToEnd, sendToText.length);
+
+      console.log({ sendToText, sendToNewText, sendToUid, currentBlockUid });
+      if (sendType === 'raw') {
+        window.roamAlphaAPI.data.block.move({
+          location: {
+            'parent-uid': sendToUid,
+            order: 0,
+          },
+          block: {
+            uid: currentBlockUid,
+          },
+        });
+      } else if (sendType === 'ref') {
+        window.roamAlphaAPI.data.block.create({
+          location: {
+            'parent-uid': sendToUid,
+            order: 0,
+          },
+          block: {
+            uid: window.roamAlphaAPI.util.generateUID(),
+            string: `((${currentBlockUid}))`,
+          },
+        });
+      }
+      // Cut block ref from block (I do this after I send the block to try to hide the fact that I cut the ref from the block)
+      window.roamAlphaAPI.data.block.update({
+        block: {
+          uid: currentBlockUid,
+          string: sendToNewText,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      alert(
+        'Error in sending block; Roam probably modified CSS classes & broke the code; contact developer'
+      );
+    }
+  }, 200);
+}
+
+function setupKeyboardShortcuts(): void {
+  document.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && e.shiftKey && e.code === 'Backspace') {
+      onShortcut(archiveBlock);
+    } else if (e.ctrlKey && e.shiftKey && e.code === 'KeyX') {
+      onShortcut(refactorBlock);
+    } else if (e.ctrlKey && e.shiftKey && e.code === 'KeyD') {
+      onShortcut(resolveCompletedObject);
+    } else if (e.altKey && e.shiftKey && e.code === 'Enter') {
+      onSendBlock('raw');
+    } else if (e.ctrlKey && e.altKey && e.code === 'Enter') {
+      onSendBlock('ref');
+    }
+  });
+}
 
 runExtension(extensionId, () => {
+  //Shortcuts
+  console.log('Initializing keyboard shortcuts');
+  setupKeyboardShortcuts();
+
+  // Context Menu
+  console.log('Initializing block context menu');
   window.roamAlphaAPI.ui.blockContextMenu.addCommand({
     label: 'Archive Block',
     callback: (e) => {
